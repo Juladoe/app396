5d824575f78b2c93112c40a144fbb4ff7e7d603f
Xiaobo Zhuge <zhugexiaobo@howzhi.com>
2014-06-06 11:09:39 +0800

    feature: ImagePlugin test.

---
+++ ios/Phonegap_ios/Plugins/ImagePlugin.h
@@ -0,0 +1,13 @@
+//
+//  ImagePlugin.h
+//  Phonegap_ios
+//
+//  Created by Edusoho on 14-6-6.
+//
+//
+
+#import <Cordova/Cordova.h>
+
+@interface ImagePlugin : CDVPlugin
+
+@end
---
+++ ios/Phonegap_ios/Plugins/ImagePlugin.m
@@ -0,0 +1,13 @@
+//
+//  ImagePlugin.m
+//  Phonegap_ios
+//
+//  Created by Edusoho on 14-6-6.
+//
+//
+
+#import "ImagePlugin.h"
+
+@implementation ImagePlugin
+
+@end
