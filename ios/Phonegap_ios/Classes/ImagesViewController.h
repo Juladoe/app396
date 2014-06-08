8346d2807922578c5ac1ff727e676a12e5f0c871
Xiaobo Zhuge <zhugexiaobo@howzhi.com>
2014-06-08 16:32:19 +0800

    feature: native plugin to view images.

---
+++ ios/Phonegap_ios/Classes/ImagesViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesViewController.h
+//  Phonegap_ios
+//
+//  Created by Edusoho on 14-6-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesViewController : UIViewController
+
+- (instancetype)initWithImageUrls:(NSArray *)urls atIndex:(NSInteger)index;
+
+@end
--- ios/Phonegap_ios/Plugins/ImagePlugin.m
+++ ios/Phonegap_ios/Plugins/ImagePlugin.m
@@ -7,16 +7,34 @@
 //
 
 #import "ImagePlugin.h"
+#import "ImagesViewController.h"
+#import "XHImageViewer.h"
+#import "UIImageView+XHURLDownload.h"
+
+@interface ImagePlugin () <XHImageViewerDelegate>
+
+
+
+@end
 
 @implementation ImagePlugin
 
-- (void)showImage:(CDVInvokedUrlCommand *)command {
+- (void)showImage:(CDVInvokedUrlCommand *)command
+{
     CDVPluginResult* pluginResult = nil;
     
-    for (id object in command.arguments) {
-        BOOL isArray = [object isKindOfClass:[NSArray class]];
-        NSLog(@"%d", isArray);
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:isArray];
+    NSInteger index = [[command.arguments objectAtIndex:0] integerValue];
+    NSArray *urls = [command.arguments objectAtIndex:1];
+    
+    if ((index >=0) && urls) {
+        ImagesViewController *imageViewer = [[ImagesViewController alloc] initWithImageUrls:urls
+                                                                                    atIndex:index];
+        [[[UIApplication sharedApplication] delegate].window.rootViewController presentViewController:imageViewer
+                                                                                                 animated:YES
+                                                                                               completion:nil];
+    }
+    else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }
     
