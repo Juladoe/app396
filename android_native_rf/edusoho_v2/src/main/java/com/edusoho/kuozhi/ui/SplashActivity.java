558fbfb0ab36abdb8df11c8b40a6d406bdee8bb1
Su ju <suju@howzhi.com>
2014-10-16 19:34:43 +0800

    update

--- android_native_rf/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/SplashActivity.java
+++ android_native_rf/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/SplashActivity.java
@@ -44,7 +44,7 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-        mPadding = (int)(EdusohoApp.screenW * 0.3f);
+        mPadding = (int)(EdusohoApp.screenW * 0.4f);
         Log.d(null, "padding->" + mPadding);
         loadConfig();
         setupJazziness(mSplashMode);
