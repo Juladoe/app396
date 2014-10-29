878af53e7131a64852e77d5ef9a6eacedf104dad
Huang Boyi <huangboyi@howzhi.com>
2014-10-29 09:48:58 +0800

    delete import

--- android_native_rf/edusoho_v2/src/main/java/com/edusoho/kuozhi/EdusohoApp.java
+++ android_native_rf/edusoho_v2/src/main/java/com/edusoho/kuozhi/EdusohoApp.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
