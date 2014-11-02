3fbd61a30c2c2cba56b8ca35cd12cadaaacb2cd8
Huang Boyi <huangboyi@howzhi.com>
2014-11-02 21:25:47 +0800

    RichTextBox,Add image with align center

--- android_native_rf/edusoho_v2/src/main/java/com/edusoho/kuozhi/util/AppUtil.java
+++ android_native_rf/edusoho_v2/src/main/java/com/edusoho/kuozhi/util/AppUtil.java
@@ -8,8 +8,6 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
@@ -571,9 +569,6 @@ public class AppUtil {
         matrix.postRotate((float) degree);
 
         Bitmap scaledBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
-//        width = scaledBitmap.getWidth(); // re-use
-//        height = scaledBitmap.getHeight(); // re-use
-        BitmapDrawable result = new BitmapDrawable(scaledBitmap);
 
         return scaledBitmap;
     }
