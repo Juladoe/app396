2f1495b2d97fea3ba308922ae932d82275826671
HuangBoyi <huangboyi@howzhi.com>
2014-12-05 22:57:12 +0800

    hide note content link to lesson_activity icon.

--- EduSohoAndroidClient/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/note/NoteContentFragment.java
+++ EduSohoAndroidClient/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/note/NoteContentFragment.java
@@ -86,29 +86,29 @@ public class NoteContentFragment extends BaseFragment {
          * 跳转到课时页面
          */
         mLessonEntrance = (ImageView) view.findViewById(R.id.lesson_entrance);
-        mLessonEntrance.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mActivity.getCoreEngine().runNormalPlugin(
-                        LessonActivity.TAG, mActivity, new PluginRunCallback() {
-                            @Override
-                            public void setIntentDate(Intent startIntent) {
-                                if (mLearnStatus == "finished") {
-                                    isLearned = true;
-                                }
-                                startIntent.putExtra(Const.COURSE_ID, mCourseId);
-//                                startIntent.putExtra(Const.FREE, );
-                                startIntent.putExtra(Const.LESSON_ID, mLessonId);
-//                                startIntent.putExtra(Const.LESSON_TYPE, lesson.type);
-//                                startIntent.putExtra(Const.ACTIONBAT_TITLE, mLessonTitle);
-//                                startIntent.putExtra(Const.LIST_JSON, mLessonListJson);
-                                startIntent.putExtra(Const.IS_LEARN, isLearned);
-                            }
-                        }
-
-                );
-            }
-        });
+//        mLessonEntrance.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mActivity.getCoreEngine().runNormalPlugin(
+//                        LessonActivity.TAG, mActivity, new PluginRunCallback() {
+//                            @Override
+//                            public void setIntentDate(Intent startIntent) {
+//                                if (mLearnStatus == "finished") {
+//                                    isLearned = true;
+//                                }
+//                                startIntent.putExtra(Const.COURSE_ID, mCourseId);
+////                                startIntent.putExtra(Const.FREE, );
+//                                startIntent.putExtra(Const.LESSON_ID, mLessonId);
+////                                startIntent.putExtra(Const.LESSON_TYPE, lesson.type);
+////                                startIntent.putExtra(Const.ACTIONBAT_TITLE, mLessonTitle);
+////                                startIntent.putExtra(Const.LIST_JSON, mLessonListJson);
+//                                startIntent.putExtra(Const.IS_LEARN, isLearned);
+//                            }
+//                        }
+//
+//                );
+//            }
+//        });
 
         setContent();
     }
