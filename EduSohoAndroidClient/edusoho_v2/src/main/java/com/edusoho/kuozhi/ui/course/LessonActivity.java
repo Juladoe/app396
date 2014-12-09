62d164ca644101fd7afac9c2b6b3b5309ce68046
HuangBoyi <huangboyi@howzhi.com>
2014-12-09 10:50:40 +0800

    lessonActivity remove menuDrawer

--- EduSohoAndroidClient/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/course/LessonActivity.java
+++ EduSohoAndroidClient/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/course/LessonActivity.java
@@ -75,14 +75,14 @@ public class LessonActivity extends ActionBarBaseActivity implements MessageEngi
     private int mLessonId;
     private String mLessonType;
     private String mTitle;
-    private String mLessonListJson;
+    //private String mLessonListJson;
     private Bundle fragmentData;
     private int mIsFree;
     private boolean mIsLearn;
     private LessonStatus mLessonStatus;
 
     protected MenuDrawer mMenuDrawer;
-    private CourseDetailsLessonWidget mCourseLessonView;
+    //private CourseDetailsLessonWidget mCourseLessonView;
     private EduSohoTextBtn mLearnBtn;
     private EduSohoTextBtn mNoteBtn;
     private EduSohoTextBtn mQuestionBtn;
@@ -192,69 +192,72 @@ public class LessonActivity extends ActionBarBaseActivity implements MessageEngi
                     return;
                 }
 
-                mCourseLessonView.updateLessonStatus(mLessonId, result);
+                //mCourseLessonView.updateLessonStatus(mLessonId, result);
                 setLearnStatus(result);
             }
         });
     }
 
     private void loadLessonList() {
-        mCourseLessonView = (CourseDetailsLessonWidget) LayoutInflater.from(mActivity).inflate(
-                R.layout.course_details_lesson_content, null);
-        mMenuDrawer.setMenuView(mCourseLessonView);
-
-        mCourseLessonView.initLessonFromJson(mActivity, mLessonListJson);
-        mCourseLessonView.setItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mMenuDrawer.closeMenu();
-                LessonItem lesson = (LessonItem) adapterView.getItemAtPosition(i);
-                hieToolsByAnim();
-                mLessonId = lesson.id;
-                mLessonType = lesson.type;
-                setTitle(lesson.title);
-                loadLesson(mLessonId);
-                if (!mLessonType.equals("testpaper") && mIsLearn) {
-                    Log.d(null, "load status->");
-                    loadLessonStatus();
-                }
-            }
-        });
+//        mCourseLessonView = (CourseDetailsLessonWidget) LayoutInflater.from(mActivity).inflate(
+//                R.layout.course_details_lesson_content, null);
+//        mMenuDrawer.setMenuView(mCourseLessonView);
+//
+//        mCourseLessonView.initLessonFromJson(mActivity, mLessonListJson);
+//        mCourseLessonView.setItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                mMenuDrawer.closeMenu();
+//                LessonItem lesson = (LessonItem) adapterView.getItemAtPosition(i);
+//                hieToolsByAnim();
+//                mLessonId = lesson.id;
+//                mLessonType = lesson.type;
+//                setTitle(lesson.title);
+//                loadLesson(mLessonId);
+//                if (!mLessonType.equals("testpaper") && mIsLearn) {
+//                    Log.d(null, "load status->");
+//                    loadLessonStatus();
+//                }
+//            }
+//        });
     }
 
     private void initView() {
-        Intent data = getIntent();
-        if (data != null) {
-            mCourseId = data.getIntExtra(Const.COURSE_ID, 0);
-            mIsFree = data.getIntExtra(Const.FREE, 0);
-            mIsLearn = data.getBooleanExtra(Const.IS_LEARN, false);
-            mLessonId = data.getIntExtra(Const.LESSON_ID, 0);
-            mTitle = data.getStringExtra(Const.ACTIONBAT_TITLE);
-            mLessonType = data.getStringExtra(Const.LESSON_TYPE);
-            mLessonListJson = data.getStringExtra(Const.LIST_JSON);
+        try {
+            Intent data = getIntent();
+            if (data != null) {
+                mCourseId = data.getIntExtra(Const.COURSE_ID, 0);
+                mIsFree = data.getIntExtra(Const.FREE, 0);
+                mIsLearn = data.getBooleanExtra(Const.IS_LEARN, false);
+                mLessonId = data.getIntExtra(Const.LESSON_ID, 0);
+                mTitle = data.getStringExtra(Const.ACTIONBAT_TITLE);
+                mLessonType = data.getStringExtra(Const.LESSON_TYPE);
+                //mLessonListJson = data.getStringExtra(Const.LIST_JSON);
+            }
+
+            setBackMode(BACK, mTitle);
+            mToolsLayout = findViewById(R.id.lesson_tools_layout);
+            mNoteBtn = (EduSohoTextBtn) findViewById(R.id.lesson_note_btn);
+            mQuestionBtn = (EduSohoTextBtn) findViewById(R.id.lesson_question_btn);
+            mMoreBtn = (EduSohoTextBtn) findViewById(R.id.lesson_more_btn);
+            mLearnBtn = (EduSohoTextBtn) findViewById(R.id.lesson_learn_btn);
+
+            loadLessonList();
+            if (mCourseId == 0 || mLessonId == 0) {
+                longToast("课程数据错误！");
+                return;
+            }
+
+            loadLesson(mLessonId);
+
+            if (!mLessonType.equals("testpaper") && mIsLearn) {
+                Log.d(null, "load status->");
+                loadLessonStatus();
+            }
+            bindListener();
+        } catch (Exception ex) {
+            Log.e("lessonActivity", ex.toString());
         }
-
-        setBackMode(BACK, mTitle);
-        mToolsLayout = findViewById(R.id.lesson_tools_layout);
-        mNoteBtn = (EduSohoTextBtn) findViewById(R.id.lesson_note_btn);
-        mQuestionBtn = (EduSohoTextBtn) findViewById(R.id.lesson_question_btn);
-        mMoreBtn = (EduSohoTextBtn) findViewById(R.id.lesson_more_btn);
-        mLearnBtn = (EduSohoTextBtn) findViewById(R.id.lesson_learn_btn);
-
-        loadLessonList();
-        if (mCourseId == 0 || mLessonId == 0) {
-            longToast("课程数据错误！");
-            return;
-        }
-
-        loadLesson(mLessonId);
-
-        if (!mLessonType.equals("testpaper") && mIsLearn) {
-            Log.d(null, "load status->");
-            loadLessonStatus();
-        }
-
-        bindListener();
     }
 
     private void showMoreBtn(View parent) {
@@ -414,7 +417,7 @@ public class LessonActivity extends ActionBarBaseActivity implements MessageEngi
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.lesson_menu_list) {
-            mMenuDrawer.openMenu();
+            //mMenuDrawer.openMenu();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -422,7 +425,7 @@ public class LessonActivity extends ActionBarBaseActivity implements MessageEngi
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         if (mIsLearn) {
-            getMenuInflater().inflate(R.menu.lesson_menu, menu);
+            //getMenuInflater().inflate(R.menu.lesson_menu, menu);
         }
         return super.onCreateOptionsMenu(menu);
     }
