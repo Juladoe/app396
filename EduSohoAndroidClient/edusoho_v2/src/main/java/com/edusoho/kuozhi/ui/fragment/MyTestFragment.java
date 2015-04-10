14876abaf57b2ba20bb8bda967b5b6203eec52cb
Huang Boyi <huangboyi@howzhi.com>
2015-04-10 17:28:27 +0800

    reformat

--- EduSohoAndroidClient/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/fragment/MyTestFragment.java
+++ EduSohoAndroidClient/edusoho_v2/src/main/java/com/edusoho/kuozhi/ui/fragment/MyTestFragment.java
@@ -20,9 +20,6 @@ import java.util.HashMap;
 
 import library.PullToRefreshBase;
 
-
-
-
 public class MyTestFragment extends BaseFragment {
 
     public static final String TITLE = "title";
@@ -50,11 +47,11 @@ public class MyTestFragment extends BaseFragment {
     protected void initView(View view) {
 
         mLoadView = view.findViewById(R.id.load_layout);
-        mCourseListView =(RefreshListWidget) view.findViewById(R.id.my_test_list);
+        mCourseListView = (RefreshListWidget) view.findViewById(R.id.my_test_list);
         mCourseListView.setMode(PullToRefreshBase.Mode.BOTH);
         mCourseListView.setAdapter(new MyTestListAdapter(
                 mActivity, R.layout.my_test_item));
-        mCourseListView.setEmptyText(new String[] { "暂无考试" });
+        mCourseListView.setEmptyText(new String[]{"暂无考试"});
 
         mCourseListView.setUpdateListener(new RefreshListWidget.UpdateListener() {
             @Override
@@ -71,14 +68,13 @@ public class MyTestFragment extends BaseFragment {
         loadCourseFromNet(0);
     }
 
-    private void loadCourseFromNet(int start)
-    {
+    private void loadCourseFromNet(int start) {
         RequestUrl url = app.bindUrl(Const.MY_TESTPAPER, true);
         HashMap<String, String> params = url.getParams();
         params.put("start", start + "");
         params.put("limit", Const.LIMIT + "");
 
-        mActivity.ajaxPost(url, new ResultCallback(){
+        mActivity.ajaxPost(url, new ResultCallback() {
             @Override
             public void callback(String url, String object, AjaxStatus ajaxStatus) {
                 mLoadView.setVisibility(View.GONE);
