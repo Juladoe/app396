a855b0b8da4ee5b7cdd20546b95f2dd844dd7634
Huang Boyi <huangboyi@howzhi.com>
2015-05-04 14:30:53 +0800

    del comment

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/FragmentNavigationDrawer.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/FragmentNavigationDrawer.java
@@ -103,10 +103,6 @@ public class FragmentNavigationDrawer extends BaseFragment {
             mRadioButtons[i] = (RadioButton) getView().findViewById(mRadioIds[i]);
             mRadioButtons[i].setOnClickListener(mRadioBtnClickListener);
         }
-//        for (int i = 0; i < mBadges.length; i++) {
-//            mBadges[i] = (BadgeView) getView().findViewById(mBadgeIds[i]);
-//
-//        }
     }
 
     View.OnClickListener mRadioBtnClickListener = new View.OnClickListener() {
