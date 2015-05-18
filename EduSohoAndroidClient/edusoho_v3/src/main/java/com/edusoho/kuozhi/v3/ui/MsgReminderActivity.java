98cf2ea692097d24042ddc1242acb4ea05930836
Huang Boyi <huangboyi@howzhi.com>
2015-05-18 14:07:18 +0800

    add checkbutton

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/MsgReminderActivity.java
@@ -0,0 +1,15 @@
+package com.edusoho.kuozhi.v3.ui;
+
+import android.os.Bundle;
+
+import com.edusoho.kuozhi.v3.ui.base.ActionBarBaseActivity;
+
+/**
+ * Created by JesseHuang on 15/5/18.
+ */
+public class MsgReminderActivity extends ActionBarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
