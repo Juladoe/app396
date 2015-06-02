d615baff6047dbd1ab41640a5c8621bef2f581b9
Melomelon <melomelon@howzhi.com>
2015-06-02 14:33:27 +0800

    friend module

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/model/bal/Friend.java
@@ -0,0 +1,13 @@
+package com.edusoho.kuozhi.v3.model.bal;
+
+/**
+ * Created by Melomelon on 2015/6/2.
+ */
+public class Friend {
+    public Friend(String name) {
+        this.name = name;
+    }
+
+    public String avatar;
+    public String name;
+}
