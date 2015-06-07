1c7ef84bd01bd62a35e8be323baf34a6e42724e2
Huang Boyi <huangboyi@howzhi.com>
2015-06-07 21:58:20 +0800

    add

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/NewsFragment.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/NewsFragment.java
@@ -2,16 +2,17 @@ package com.edusoho.kuozhi.v3.ui.fragment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.edusoho.kuozhi.R;
-import com.edusoho.kuozhi.v3.adapter.NewsAdapter;
+import com.edusoho.kuozhi.v3.adapter.SwipeAdapter;
 import com.edusoho.kuozhi.v3.listener.PluginRunCallback;
 import com.edusoho.kuozhi.v3.model.InitModelTool;
 import com.edusoho.kuozhi.v3.model.bal.news.NewsEnum;
@@ -20,7 +21,12 @@ import com.edusoho.kuozhi.v3.model.sys.MessageType;
 import com.edusoho.kuozhi.v3.model.sys.WidgetMessage;
 import com.edusoho.kuozhi.v3.ui.ChatActivity;
 import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
+import com.edusoho.kuozhi.v3.util.AppUtil;
 import com.edusoho.kuozhi.v3.util.Const;
+import com.edusoho.kuozhi.v3.view.swipemenulistview.SwipeMenu;
+import com.edusoho.kuozhi.v3.view.swipemenulistview.SwipeMenuCreator;
+import com.edusoho.kuozhi.v3.view.swipemenulistview.SwipeMenuItem;
+import com.edusoho.kuozhi.v3.view.swipemenulistview.SwipeMenuListView;
 
 import java.util.List;
 
@@ -29,8 +35,8 @@ import java.util.List;
  * 动态列表
  */
 public class NewsFragment extends BaseFragment {
-    private ListView lvChatList;
-    private NewsAdapter mNewsAdapter;
+    private SwipeMenuListView lvNewsList;
+    private SwipeAdapter mSwipeAdapter;
 
     @Override
     public void onAttach(Activity activity) {
@@ -64,17 +70,59 @@ public class NewsFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
-        lvChatList = (ListView) view.findViewById(R.id.lv_chat_list);
         List<NewsItem> list = InitModelTool.initNewsItemList();
 //        if (app.loginUser.nickname.equals("suju")) {
 //            list = InitModelTool.initNewsItemList1();
 //        } else {
 //            list = InitModelTool.initNewsItemList();
 //        }
-        mNewsAdapter = new NewsAdapter(mContext, R.layout.news_item, list);
-        lvChatList.setAdapter(mNewsAdapter);
-        lvChatList.setOnItemClickListener(mItemClickListener);
+
+        lvNewsList = (SwipeMenuListView) view.findViewById(R.id.lv_news_list);
+        mSwipeAdapter = new SwipeAdapter(mContext, R.layout.news_item, list);
+        lvNewsList.setAdapter(mSwipeAdapter);
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+//                SwipeMenuItem openItem = new SwipeMenuItem(
+//                        mContext);
+//                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+//                        0xCE)));
+//                openItem.setWidth(AppUtil.dp2px(mContext, 90));
+//                openItem.setTitle("标为未读");
+//                openItem.setTitleSize(18);
+//                openItem.setTitleColor(Color.WHITE);
+//                menu.addMenuItem(openItem);
+
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        mContext);
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                deleteItem.setWidth(AppUtil.dp2px(mContext, 90));
+                deleteItem.setIcon(R.drawable.ic_delete);
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        lvNewsList.setMenuCreator(creator);
+        lvNewsList.setOnMenuItemClickListener(mMenuItemClickListener);
+        lvNewsList.setOnItemClickListener(mItemClickListener);
     }
+
+    SwipeMenuListView.OnMenuItemClickListener mMenuItemClickListener = new SwipeMenuListView.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+            switch (index) {
+                case 0:
+                    mSwipeAdapter.removeItem(position);
+                    mSwipeAdapter.notifyDataSetChanged();
+                    // TODO 本地数据库操作
+                    break;
+                case 1:
+                    break;
+            }
+            return false;
+        }
+    };
 
     AdapterView.OnItemClickListener mItemClickListener = new AdapterView.OnItemClickListener() {
 
@@ -96,7 +144,7 @@ public class NewsFragment extends BaseFragment {
 
     private void getNewOneMessage(Bundle data) {
         NewsItem item = (NewsItem) data.getSerializable("msg");
-        mNewsAdapter.findNews(item);
+        //mSwipeAdapter.findNews(item);
     }
 
     @Override
@@ -108,12 +156,10 @@ public class NewsFragment extends BaseFragment {
         }
     }
 
-
     @Override
     public MessageType[] getMsgTypes() {
         String source = this.getClass().getSimpleName();
         MessageType[] messageTypes = new MessageType[]{new MessageType(Const.CHAT_MSG, source)};
         return messageTypes;
     }
-
 }
