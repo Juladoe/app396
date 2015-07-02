d740e45a15b3175628a3c69cc9ec2ff99bbafd37
Huang Boyi <huangboyi@howzhi.com>
2015-07-02 11:34:08 +0800

    fix

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/model/result/PushResult.java
@@ -0,0 +1,8 @@
+package com.edusoho.kuozhi.v3.model.result;
+
+/**
+ * Created by howzhi on 14-5-25.
+ */
+public class PushResult {
+    public String result;
+}
---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/model/result/SchoolResult.java
@@ -0,0 +1,10 @@
+package com.edusoho.kuozhi.v3.model.result;
+
+import com.edusoho.kuozhi.v3.model.sys.School;
+
+/**
+ * Created by howzhi on 14-5-25.
+ */
+public class SchoolResult {
+    public School site;
+}
