825596cc28cfe7d174b208640a4f614b7ae09966
Su ju <suju@howzhi.com>
2015-07-08 10:38:07 +0800

    fix cordova plugin

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/view/webview/bridge/CallbackStatus.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/view/webview/bridge/CallbackStatus.java
@@ -3,21 +3,21 @@ package com.edusoho.kuozhi.v3.view.webview.bridge;
 /**
  * Created by howzhi on 15/4/17.
  */
-public class CallbackStatus {
+public class CallbackStatus<T> {
 
     public static final int SUCCESS = 0;
     public static final int ERROR = -1;
 
     protected int status;
-    protected Object message;
+    protected T message;
 
-    public void setSuccess(Object message)
+    public void setSuccess(T message)
     {
         this.status = SUCCESS;
         this.message = message;
     }
 
-    public void setError(Object message)
+    public void setError(T message)
     {
         this.status = ERROR;
         this.message = message;
@@ -28,7 +28,7 @@ public class CallbackStatus {
         return status;
     }
 
-    public Object getMessage()
+    public T getMessage()
     {
         return message;
     }
--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/view/webview/bridge/CoreBridge.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/view/webview/bridge/CoreBridge.java
@@ -1,9 +1,18 @@
 package com.edusoho.kuozhi.v3.view.webview.bridge;
 
 import android.content.Context;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 
+import com.edusoho.kuozhi.v3.ui.base.ActionBarBaseActivity;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -11,16 +20,47 @@ import java.util.HashMap;
 /**
  * Created by howzhi on 15/4/17.
  */
-public class CoreBridge {
+public class CoreBridge extends CordovaPlugin {
 
     public static final String TAG = "CoreBridge";
 
     protected HashMap<String, Method> mMethodList;
     protected Context mContext;
+    protected JSONArray args;
+    protected ActionBarBaseActivity mActivity;
 
-    public CoreBridge(Context context)
-    {
-        this.mContext = context;
+    public CoreBridge() {
+        super();
+        initMethods();
+    }
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        mActivity = (ActionBarBaseActivity) cordova.getActivity();
+        super.initialize(cordova, webView);
+    }
+
+    @Override
+    public boolean execute(
+            String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        CallbackStatus<JSONObject> callbackStatus = invoke(action, args);
+
+        JSONObject message = callbackStatus.getMessage();
+        switch (callbackStatus.getStatus()) {
+            case CallbackStatus.ERROR:
+                callbackContext.equals(message);
+                break;
+            case CallbackStatus.SUCCESS:
+                if (message != null) {
+                    callbackContext.success(message);
+                }
+                break;
+        }
+
+        return super.execute(action, args, callbackContext);
+    }
+
+    private void initMethods() {
         mMethodList = new HashMap<String, Method>();
         try {
             Method[] methods = this.getClass().getMethods();
@@ -32,24 +72,49 @@ public class CoreBridge {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.d(TAG, e.toString());
         }
     }
 
-    public CallbackStatus invoke(String action, JSONArray args)
+    public CallbackStatus<JSONObject> invoke(String action, JSONArray args)
     {
+        this.args = args;
         CallbackStatus callbackStatus = new CallbackStatus();
         Method method = mMethodList.get(action);
         if (method != null) {
             try {
-                Object object = method.invoke(this, args);
+                JSONObject object = (JSONObject) method.invoke(this, null);
                 callbackStatus.setSuccess(object);
             } catch (Exception e) {
-                callbackStatus.setError(e);
-                e.printStackTrace();
+                callbackStatus.setError(getInvokeError(e));
+                Log.d(TAG, e.toString());
             }
         }
 
         return callbackStatus;
+    }
+
+    private JSONObject getInvokeError(Exception error) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("error" , error.getMessage());
+        }catch (Exception e) {
+        }
+
+        return jsonObject;
+    }
+
+    protected String[] JsonArrayToStringArray(JSONArray images)
+    {
+        int length = images.length();
+        String[] strs = new String[length];
+        try {
+            for (int i=0; i < length; i++) {
+                strs[i] = images.getString(i);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return strs;
     }
 }
