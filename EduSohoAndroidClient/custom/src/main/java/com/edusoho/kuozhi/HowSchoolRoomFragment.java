2df53c2212ce3ea8528e73e4d7863b0580993e59
Su ju <suju@howzhi.com>
2015-07-11 11:40:49 +0800

    fix howzhi app

---
+++ EduSohoAndroidClient/custom/src/main/java/com/edusoho/kuozhi/HowSchoolRoomFragment.java
@@ -0,0 +1,26 @@
+package com.edusoho.kuozhi;
+
+import android.util.Log;
+
+import com.edusoho.kuozhi.model.SchoolRoom.SchoolRoomResult;
+import com.edusoho.kuozhi.ui.fragment.SchoolRoomFragment;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by howzhi on 15/7/10.
+ */
+public class HowSchoolRoomFragment extends SchoolRoomFragment {
+
+    @Override
+    protected void filterSchoolRoomDatas(List<SchoolRoomResult> list) {
+        Iterator<SchoolRoomResult> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String title = iterator.next().title;
+            if (title.equals("讨论") || title.equals("笔记")) {
+                iterator.remove();
+            }
+        }
+    }
+}
