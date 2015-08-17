0802530893d6b5d66a902f702cae4e924c52656e
Huang Boyi <huangboyi@howzhi.com>
2015-08-17 16:48:04 +0800

    fix

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/model/Result/FollowResult.java
+++
@@ -1,8 +0,0 @@
-package com.edusoho.kuozhi.v3.model.result;
-
-/**
- * Created by Melomelon on 2015/7/2.
- */
-public class FollowResult {
-    public boolean success;
-}
--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/model/result/PushResult.java
+++
@@ -1,9 +0,0 @@
-package com.edusoho.kuozhi.v3.model.result;
-
-/**
- * Created by howzhi on 14-5-25.
- */
-public class PushResult {
-    public int id;
-    public String result;
-}
