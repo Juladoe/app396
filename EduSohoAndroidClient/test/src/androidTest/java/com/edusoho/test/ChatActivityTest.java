d6ef6a50ccafc91b772009a61f5d53ed0e011ad1
Huang Boyi <huangboyi@howzhi.com>
2015-08-22 10:56:04 +0800

    fix ChatActivityTests setUp

--- EduSohoAndroidClient/test/src/androidTest/java/com/edusoho/test/ChatActivityTest.java
+++ EduSohoAndroidClient/test/src/androidTest/java/com/edusoho/test/ChatActivityTest.java
@@ -1,5 +1,6 @@
 package com.edusoho.test;
 
+import android.content.Intent;
 import android.test.UiThreadTest;
 
 import com.edusoho.kuozhi.v3.ui.ChatActivity;
@@ -12,6 +13,12 @@ public class ChatActivityTest extends BaseActivityUnitTestCase<ChatActivity> {
 
     public ChatActivityTest() {
         super(ChatActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mLaunchIntent = new Intent(mInstrumentation.getTargetContext(), ChatActivity.class);
     }
 
     @UiThreadTest
