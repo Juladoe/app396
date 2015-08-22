bcf99389ad24d3c6a7863bfea7b0bd6a291bbaa2
Huang Boyi <huangboyi@howzhi.com>
2015-08-22 22:46:19 +0800

    add ChatActivityTest UI test

--- EduSohoAndroidClient/test/src/androidTest/java/com/edusoho/test/ChatActivityTest.java
+++ EduSohoAndroidClient/test/src/androidTest/java/com/edusoho/test/ChatActivityTest.java
@@ -5,8 +5,16 @@ import android.content.Intent;
 import android.test.ActivityUnitTestCase;
 import android.test.UiThreadTest;
 import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.edusoho.kuozhi.v3.ui.ChatActivity;
+import com.edusoho.kuozhi.v3.view.EduSohoIconView;
+
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
 
 /**
  * Created by JesseHuang on 15/8/18.
@@ -53,5 +61,45 @@ public class ChatActivityTest extends ActivityUnitTestCase<ChatActivity> {
         assertNotNull(mChatActivity);
     }
 
+    @UiThreadTest
+    public void testInitUI() {
+        mChatActivity = getActivity();
+        ListView listView = (ListView) mChatActivity.findViewById(R.id.lv_messages);
+        assertNotNull(listView);
+
+        PtrClassicFrameLayout ptrClassicFrameLayout = (PtrClassicFrameLayout) mChatActivity.findViewById(R.id.rotate_header_list_view_frame);
+        assertNotNull(ptrClassicFrameLayout);
+
+        View viewSpeakContainer = mChatActivity.findViewById(R.id.recording_container);
+        assertNotNull(viewSpeakContainer);
+        assertEquals(View.GONE, viewSpeakContainer.getVisibility());
+        ImageView ivRecordImage = (ImageView) mChatActivity.findViewById(R.id.iv_voice_volume);
+        assertNotNull(ivRecordImage);
+        TextView tvSpeakHint = (TextView) mChatActivity.findViewById(R.id.tv_speak_hint);
+        assertNotNull(tvSpeakHint);
+
+        View viewChatDialog = mChatActivity.findViewById(R.id.ll_chat_dialog);
+        assertNotNull(viewChatDialog);
+        EduSohoIconView btnVoice = (EduSohoIconView) mChatActivity.findViewById(R.id.btn_voice);
+        assertNotNull(btnVoice);
+        assertEquals(mChatActivity.getResources().getString(R.string.font_chat_voice), btnVoice.getText());
+        EduSohoIconView btnKeyBoard = (EduSohoIconView) mChatActivity.findViewById(R.id.btn_set_mode_keyboard);
+        assertNotNull(btnKeyBoard);
+        View viewPressToSpeak = mChatActivity.findViewById(R.id.rl_btn_press_to_speak);
+        assertNotNull(viewPressToSpeak);
+        assertEquals(View.GONE, viewPressToSpeak.getVisibility());
+        TextView tvSpeak = (TextView) mChatActivity.findViewById(R.id.tv_speak);
+        assertNotNull(tvSpeak);
+        assertEquals(mChatActivity.getResources().getString(R.string.hand_press_and_speak), tvSpeak.getText());
+        View viewMsgInput = mChatActivity.findViewById(R.id.rl_msg_input);
+        assertNotNull(viewMsgInput);
+        assertEquals(View.VISIBLE, viewMsgInput.getVisibility());
+        EduSohoIconView ivAddMedia = (EduSohoIconView) mChatActivity.findViewById(R.id.iv_show_media_layout);
+        assertNotNull(ivAddMedia);
+        assertEquals(mChatActivity.getResources().getString(R.string.font_chat_add_media), ivAddMedia.getText());
+        Button btnSend = (Button) mChatActivity.findViewById(R.id.tv_send);
+        assertNotNull(btnSend);
+        assertEquals(View.GONE, btnSend.getVisibility());
+    }
 
 }
