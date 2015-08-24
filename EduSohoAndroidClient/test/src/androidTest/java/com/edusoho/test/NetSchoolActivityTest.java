bf9ea0e5b7c7462bcdcf45eeccaa67dd19068b66
Huang Boyi <huangboyi@howzhi.com>
2015-08-24 15:09:08 +0800

    add NetSchoolActivityTest

---
+++ EduSohoAndroidClient/test/src/androidTest/java/com/edusoho/test/NetSchoolActivityTest.java
@@ -0,0 +1,32 @@
+package com.edusoho.test;
+
+import android.test.UiThreadTest;
+import android.view.View;
+
+import com.edusoho.kuozhi.v3.ui.NetSchoolActivity;
+import com.edusoho.kuozhi.v3.view.EdusohoAutoCompleteTextView;
+
+/**
+ * Created by JesseHuang on 15/8/24.
+ */
+public class NetSchoolActivityTest extends BaseActivityUnitTestCase<NetSchoolActivity> {
+    public NetSchoolActivityTest() {
+        super(NetSchoolActivity.class);
+    }
+
+    @UiThreadTest
+    public void testNetSchoolActivity() {
+        NetSchoolActivity mActivity = getActivity();
+        assertNotNull(mActivity);
+    }
+
+    @UiThreadTest
+    public void testLayout() {
+        NetSchoolActivity mActivity = getActivity();
+        View mSearchBtn = mActivity.findViewById(R.id.normal_search_btn);
+        assertNotNull(mSearchBtn);
+        EdusohoAutoCompleteTextView mSearchEdt = (EdusohoAutoCompleteTextView) mActivity.findViewById(R.id.school_url_edit);
+        assertNotNull(mSearchEdt);
+
+    }
+}
