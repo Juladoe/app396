a54c0db1aa963969f9739149039263bdff42161e
Su ju <suju@howzhi.com>
2015-09-09 18:47:20 +0800

    add ArticleFragment

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/article/ArticleFragment.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/article/ArticleFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
 import com.edusoho.kuozhi.R;
+import com.edusoho.kuozhi.v3.listener.NormalCallback;
+import com.edusoho.kuozhi.v3.model.bal.article.MenuItem;
+import com.edusoho.kuozhi.v3.model.provider.ArticleProvider;
+import com.edusoho.kuozhi.v3.model.sys.RequestUrl;
 import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
 
+import java.util.List;
 import in.srain.cube.views.ptr.PtrClassicFrameLayout;
 
 /**
@@ -15,9 +19,10 @@ import in.srain.cube.views.ptr.PtrClassicFrameLayout;
  */
 public class ArticleFragment extends BaseFragment {
 
-    private ListView mMessageListView;
-    private ViewGroup mMenuLayout;
-    private PtrClassicFrameLayout mMessageLayout;
+    private ArticleProvider mArticleProvider;
+    protected ListView mMessageListView;
+    protected ViewGroup mMenuLayout;
+    protected PtrClassicFrameLayout mMessageLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,5 +42,15 @@ public class ArticleFragment extends BaseFragment {
         mMenuLayout = (ViewGroup) view.findViewById(R.id.message_menu_layout);
         mMessageListView = (ListView) view.findViewById(R.id.message_list);
         mMessageLayout = (PtrClassicFrameLayout) view.findViewById(R.id.message_list_layout);
+    }
+
+    private void initMenu() {
+        RequestUrl requestUrl = new RequestUrl();
+        mArticleProvider.getMenus(requestUrl).success(new NormalCallback<List<MenuItem>>() {
+            @Override
+            public void success(List<MenuItem> menuItems) {
+
+            }
+        });
     }
 }
