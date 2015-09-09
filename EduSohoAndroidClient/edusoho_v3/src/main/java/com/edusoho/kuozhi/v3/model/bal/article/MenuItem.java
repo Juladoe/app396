f56305c007af94f3b46a68104de70e44409e73dd
Su ju <suju@howzhi.com>
2015-09-09 18:48:30 +0800

    add article MenuItem

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/model/bal/article/MenuItem.java
@@ -0,0 +1,12 @@
+package com.edusoho.kuozhi.v3.model.bal.article;
+
+/**
+ * Created by howzhi on 15/9/9.
+ */
+public class MenuItem {
+
+    public int id;
+    public String title;
+    public String type;
+    public String action;
+}
