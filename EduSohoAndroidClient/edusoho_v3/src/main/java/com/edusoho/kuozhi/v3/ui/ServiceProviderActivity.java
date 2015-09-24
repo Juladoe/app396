03c94763c72d6d7ee838a7813b1838573ec5bd24
Su ju <suju@howzhi.com>
2015-09-24 22:50:09 +0800

    更改ServiceProviderActivity启动方式

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/ServiceProviderActivity.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/ServiceProviderActivity.java
@@ -1,31 +1,47 @@
 package com.edusoho.kuozhi.v3.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+
+import com.edusoho.kuozhi.v3.util.ServiceProviderUtil;
 
 /**
  * Created by howzhi on 15/9/18.
  */
 public class ServiceProviderActivity extends FragmentPageActivity {
 
-    public static final String ARTICLE = "Article";
+    public static final String ARTICLE = "news";
     public static final String COURSE = "Course";
+    public static final String SERVICE_ID = "id";
+    public static final String SERVICE_TYPE = "type";
     public static String SERVICE_NAME;
 
     @Override
-    protected void loadFragment(String fragmentName, Bundle bundle) {
-        SERVICE_NAME = getServiceName(fragmentName);
-        super.loadFragment(fragmentName, bundle);
+    protected void initView() {
+        Intent data = getIntent();
+
+        String type = data.getStringExtra(SERVICE_TYPE);
+        String fragmentName = getFragmentNameByType(type);
+        SERVICE_NAME = fragmentName;
+        data.putExtra(FRAGMENT, fragmentName);
+        super.initView();
     }
 
-    private String getServiceName(String fragmentName) {
+    private static String getFragmentNameByType(String type) {
+        return ServiceProviderUtil.coverFragmentName(type);
+    }
 
-        if (TextUtils.isEmpty(fragmentName)) {
-            return null;
+    public static boolean isRunWithFragmentByType(String type) {
+        if (TextUtils.isEmpty(SERVICE_NAME)) {
+            return false;
         }
-        int serviceNameStart = fragmentName.lastIndexOf("Fragment");
-        return fragmentName.substring(0, serviceNameStart);
+        if (SERVICE_NAME.equals(getFragmentNameByType(type))) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
