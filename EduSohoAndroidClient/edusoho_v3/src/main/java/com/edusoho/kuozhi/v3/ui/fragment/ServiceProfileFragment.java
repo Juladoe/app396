ac1f6b131126cbe0f0670d69a6c23cd43f84aa41
Su ju <suju@howzhi.com>
2015-09-24 22:46:52 +0800

    add ServiceProfileFragment

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/ServiceProfileFragment.java
@@ -0,0 +1,100 @@
+package com.edusoho.kuozhi.v3.ui.fragment;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.edusoho.kuozhi.R;
+import com.edusoho.kuozhi.v3.listener.NormalCallback;
+import com.edusoho.kuozhi.v3.model.bal.SchoolApp;
+import com.edusoho.kuozhi.v3.model.provider.ModelProvider;
+import com.edusoho.kuozhi.v3.model.provider.SystemProvider;
+import com.edusoho.kuozhi.v3.model.sys.RequestUrl;
+import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
+import com.edusoho.kuozhi.v3.util.CommonUtil;
+import com.edusoho.kuozhi.v3.util.Const;
+import com.edusoho.kuozhi.v3.util.sql.ChatDataSource;
+import com.edusoho.kuozhi.v3.util.sql.NewsCourseDataSource;
+import com.edusoho.kuozhi.v3.util.sql.ServiceProviderDataSource;
+import com.edusoho.kuozhi.v3.util.sql.SqliteChatUtil;
+import com.edusoho.kuozhi.v3.view.dialog.PopupDialog;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Created by howzhi on 15/9/23.
+ */
+public class ServiceProfileFragment extends BaseFragment {
+
+    public static final String SERVICE_ID = "id";
+
+    private SystemProvider mSystemProvider;
+    private ImageView mServiceIconView;
+    private TextView mServiceNameView;
+    private TextView mServiceTitleView;
+    private TextView mClearView;
+
+    private int mSchoolProfileId;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContainerView(R.layout.service_profile_layout);
+        ModelProvider.init(mContext, this);
+    }
+
+    @Override
+    protected void initView(View view) {
+        super.initView(view);
+
+        mServiceIconView = (ImageView) view.findViewById(R.id.sp_icon);
+        mServiceNameView = (TextView) view.findViewById(R.id.sp_name);
+        mServiceTitleView = (TextView) view.findViewById(R.id.sp_title);
+        mClearView = (TextView) view.findViewById(R.id.sp_icon_clear_message);
+        initServiceProfile();
+    }
+
+    private void initServiceProfile() {
+
+        Bundle bundle = getArguments();
+        mSchoolProfileId = bundle != null ? bundle.getInt(SERVICE_ID, 0) : 0;
+
+        RequestUrl requestUrl = app.bindNewUrl(String.format(Const.GET_SCHOOL_APP, mSchoolProfileId), true);
+        mSystemProvider.getSchoolApp(requestUrl).success(new NormalCallback<SchoolApp>() {
+            @Override
+            public void success(SchoolApp schoolApp) {
+                if (schoolApp == null) {
+                    CommonUtil.longToast(mContext, "获取服务号内容失败");
+                    return;
+                }
+                initSchoolApp(schoolApp);
+            }
+        });
+    }
+
+    private void initSchoolApp(SchoolApp schoolApp) {
+
+        mServiceNameView.setText(schoolApp.name);
+        mServiceTitleView.setText(schoolApp.title);
+        mClearView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupDialog.createMuilt(mActivity, "消息记录", "清除历史消息", new PopupDialog.PopupClickListener() {
+                    @Override
+                    public void onClick(int button) {
+                        if (button == PopupDialog.OK) {
+                            clearHistory();
+                        }
+                    }
+                }).show();
+            }
+        });
+        ImageLoader.getInstance().displayImage(schoolApp.avatar, mServiceIconView, app.mOptions);
+    }
+
+    private void clearHistory() {
+        ServiceProviderDataSource dataSource = new ServiceProviderDataSource(SqliteChatUtil.getSqliteChatUtil(mContext, app.domain));
+        dataSource.deleteBySPId(mSchoolProfileId);
+    }
+}
