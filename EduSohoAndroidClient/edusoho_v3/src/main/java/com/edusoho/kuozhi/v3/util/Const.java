cfc0e50365aaaa43ea8acc28af974623a73ce4f4
Huang Boyi <huangboyi@howzhi.com>
2015-10-10 11:06:53 +0800

    change push server address to tui.edusoho.net/v2

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/util/Const.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/util/Const.java
@@ -265,9 +265,7 @@ public class Const {
     public static final String USER_DATA_NUMBER = "User/getUserNum";
 
     public static final String GET_API_TOKEN = "/api/mobileschools/token";
-    public static final String PUSH_HOST = "http://121.41.107.178:80/v2";
-    //public static final String PUSH_HOST = "http://115.29.78.158:94/v2";
-    //public static final String PUSH_HOST = "http://tui.edusoho.net/v1";
+    public static final String PUSH_HOST = "http://tui.edusoho.net/v2";
     public static final String ANONYMOUS_BIND = "/auth/anonymousBind";
     public static final String BIND = "/auth/bind";
     public static final String UNBIND = "/auth/unBind";
