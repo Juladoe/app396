e1dd3da9b19c0d615dac83df0982e14401b59e35
Melomelon <melomelon@howzhi.com>
2015-10-10 16:02:09 +0800

    add lesson exercise option

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/ExerciseOptionDialog.java
@@ -0,0 +1,73 @@
+package com.edusoho.kuozhi.v3.ui;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.edusoho.kuozhi.R;
+
+/**
+ * Created by Melomelon on 2015/10/10.
+ */
+public class ExerciseOptionDialog extends Dialog {
+
+    private Context mContext;
+    private LinearLayout mLayout;
+
+    private TextView homework;
+    private TextView exercise;
+
+    public View.OnClickListener mClickListener;
+
+
+    public ExerciseOptionDialog(Context context) {
+        super(context, R.style.FullDialogTheme);
+        mContext = context;
+        setContentView(R.layout.exercise_option_layout);
+
+        initView();
+        initWindow();
+    }
+
+    public void initView() {
+        homework = (TextView) findViewById(R.id.do_homework);
+        exercise = (TextView) findViewById(R.id.do_exercise);
+        mClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = view.getId();
+                if (id == R.id.do_homework) {
+                    //todo
+                    Toast.makeText(mContext,"homework",Toast.LENGTH_SHORT);
+                }
+                if (id == R.id.do_exercise) {
+                    //todo
+                    Toast.makeText(mContext,"exercise",Toast.LENGTH_SHORT);
+                }
+            }
+        };
+        homework.setOnClickListener(mClickListener);
+        exercise.setOnClickListener(mClickListener);
+    }
+
+    public void initWindow() {
+        Window window = getWindow();
+        window.setGravity(Gravity.BOTTOM);
+
+        WindowManager.LayoutParams lp = window.getAttributes();
+        WindowManager windowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        lp.width = display.getWidth();
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.x = 0;
+
+        window.setAttributes(lp);
+    }
+}
