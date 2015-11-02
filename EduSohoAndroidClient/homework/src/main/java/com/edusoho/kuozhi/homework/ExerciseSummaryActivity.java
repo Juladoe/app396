50cfedbe1733054f4fad63d8a49fa539048ed1f8
Su ju <suju@howzhi.com>
2015-11-02 17:01:29 +0800

    add ExerciseSummaryActivity

---
+++ EduSohoAndroidClient/homework/src/main/java/com/edusoho/kuozhi/homework/ExerciseSummaryActivity.java
@@ -0,0 +1,77 @@
+package com.edusoho.kuozhi.homework;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import com.edusoho.kuozhi.homework.listener.BaseLessonPluginCallback;
+import com.edusoho.kuozhi.homework.model.ExerciseProvider;
+import com.edusoho.kuozhi.v3.model.sys.RequestUrl;
+import com.edusoho.kuozhi.v3.util.ApiTokenUtil;
+import com.edusoho.kuozhi.v3.util.CommonUtil;
+import com.edusoho.kuozhi.v3.util.Const;
+
+/**
+ * Created by howzhi on 15/11/2.
+ */
+public class ExerciseSummaryActivity extends HomeworkSummaryActivity {
+
+    private ExerciseProvider mExerciseProvider;
+
+    @Override
+    public String getType() {
+        return EXERCISE;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null) {
+            return;
+        }
+        int exerciseId = data.getIntExtra(ExerciseActivity.EXERCISE_ID, 0);
+        Bundle bundle = new Bundle();
+        bundle.putInt(ExerciseParseActivity.EXERCISE_ID,exerciseId);
+        app.mEngine.runNormalPluginWithBundle("ExerciseParseActivity",mContext,bundle);
+    }
+
+    @Override
+    protected void initView() {
+        setBackMode(BACK, "练习");
+        renderExerciseView();
+    }
+
+    private void renderExerciseView() {
+        String fragmentName = "com.edusoho.kuozhi.homework.ui.fragment.HomeWorkSummaryFragment";
+        Bundle bundle = getIntent().getExtras();
+        loadFragment(bundle, fragmentName);
+    }
+
+    public static class Callback extends BaseLessonPluginCallback
+    {
+        public Callback(Context context)
+        {
+            super(context);
+        }
+
+        @Override
+        protected RequestUrl getRequestUrl(int lessonId) {
+            String url = new StringBuilder()
+                    .append(String.format(Const.EXERCISE_CONTENT, lessonId))
+                    .append("?_idType=lesson")
+                    .toString();
+            return ApiTokenUtil.bindNewUrl(mContext, url, true);
+        }
+
+        @Override
+        public boolean click(AdapterView<?> parent, View view, int position) {
+            if (!view.isEnabled()) {
+                CommonUtil.longToast(mContext, "课程暂无练习");
+                return true;
+            }
+            return false;
+        }
+    }
+}
