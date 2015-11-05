ff43a37b03da72e76eeea8b473f658f15e2f1c18
Su ju <suju@howzhi.com>
2015-11-05 15:47:59 +0800

    rf LessonPluginCallback

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/listener/LessonPluginCallback.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/listener/LessonPluginCallback.java
@@ -3,13 +3,15 @@ package com.edusoho.kuozhi.v3.listener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 
 /**
  * Created by howzhi on 15/11/2.
  */
 public interface LessonPluginCallback {
 
-    public void initPlugin(View view, Bundle bundle);
+    public void initPlugin(BaseAdapter adapter, int postion);
 
     public boolean click(AdapterView<?> parent, View view, int position);
 }
