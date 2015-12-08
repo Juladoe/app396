9cb3f85514d4c95923af4aa24f36c4751132b096
suju <suju@howzhi.com>
2015-12-08 16:00:16 +0800

    share sdk add config

--- EduSohoAndroidClient/shard/src/main/java/com/edusoho/kuozhi/shard/ShareSDKUtil.java
+++ EduSohoAndroidClient/shard/src/main/java/com/edusoho/kuozhi/shard/ShareSDKUtil.java
@@ -1,6 +1,8 @@
 package com.edusoho.kuozhi.shard;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.util.ArrayMap;
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
@@ -9,6 +11,7 @@ import org.xmlpull.v1.XmlPullParser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.ShareSDK;
@@ -19,18 +22,39 @@ import cn.sharesdk.framework.ShareSDK;
 public class ShareSDKUtil {
 
     private HashMap<String, String> platformMap;
+    private Context mContext;
 
     public void initSDK(Context context) {
+        mContext = context;
         platformMap = new HashMap<>(10);
         ShareSDK.initSDK(context, "41f51eeb5d88");
         initDevInfo(context);
+    }
+
+    private static boolean isWeixinAvilible(Context context) {
+        final PackageManager packageManager = context.getPackageManager();// 获取packagemanager
+        List<PackageInfo> pinfo = packageManager.getInstalledPackages(0);// 获取所有已安装程序的包信息
+        if (pinfo != null) {
+            for (int i = 0; i < pinfo.size(); i++) {
+                String pn = pinfo.get(i).packageName;
+                if (pn.equals("com.tencent.mm")) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public Platform[] getPlatformList() {
         Platform[] platforms = ShareSDK.getPlatformList();
         ArrayList<Platform> platformsList = new ArrayList<>();
         for (int i=0; i < platforms.length; i++) {
-            if (platformMap.containsKey(platforms[i].getName())) {
+            String name = platforms[i].getName();
+            if ("Wechat".equals(name) && !isWeixinAvilible(mContext)) {
+                continue;
+            }
+            if (platformMap.containsKey(name)) {
                 platformsList.add(platforms[i]);
             }
         }
