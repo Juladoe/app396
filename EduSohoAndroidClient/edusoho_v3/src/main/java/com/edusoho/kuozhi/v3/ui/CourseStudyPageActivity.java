d3928f98908e2d697041ea4c89394833d9d9be8c
melomelon <melomelon@howzhi.com>
2015-12-10 10:03:17 +0800

    fix toolbar

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/CourseStudyPageActivity.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/CourseStudyPageActivity.java
@@ -55,7 +55,7 @@ public class CourseStudyPageActivity extends BaseActivity{
         mToolbar = (Toolbar) findViewById(R.id.course_study_page_toolbar);
         if (mToolbar != null){
             setSupportActionBar(mToolbar);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mToolbar.setNavigationIcon(R.drawable.icon_up_button);
 
             mTabRadioGroup = (RadioGroup) findViewById(R.id.course_process_radio_group);
             studyButton = (RadioButton) findViewById(R.id.radio_group_study_process);
