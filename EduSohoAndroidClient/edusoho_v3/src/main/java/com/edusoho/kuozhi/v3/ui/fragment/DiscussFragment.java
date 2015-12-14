3c28400e2780fed4932eca3b6d022f2f571ee9ad
Huang Boyi <huangboyi@howzhi.com>
2015-12-14 16:14:10 +0800

    add course discuss SqlDataSource and Entity

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/DiscussFragment.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/DiscussFragment.java
@@ -3,9 +3,19 @@ package com.edusoho.kuozhi.v3.ui.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.edusoho.kuozhi.R;
+import com.edusoho.kuozhi.v3.broadcast.AudioDownloadReceiver;
 import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
+import com.edusoho.kuozhi.v3.view.EduSohoIconView;
+
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
 
 /**
  * Created by JesseHuang on 15/12/14.
