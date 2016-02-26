8ebb9e651a3628a2348016c6995ae1302b4d1f34
melomelon <melomelon@howzhi.com>
2016-02-26 11:31:05 +0800

    Revert "add my thread page"
    This reverts commit 03a283a1920538d5280fa8c83c55bb9b64ee4d08.

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/MyPostedThreadFragment.java
+++
@@ -1,9 +0,0 @@
-package com.edusoho.kuozhi.v3.ui.fragment;
-
-import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
-
-/**
- * Created by melomelon on 16/2/25.
- */
-public class MyPostedThreadFragment extends BaseFragment {
-}
