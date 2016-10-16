36375008130ec385d3edfe1e0f397ea4ccb3d012
suju <suju@howzhi.com>
2016-10-16 18:27:07 +0800

    视频播放增加遮罩

---
+++ EduSohoAndroidClient/liveplayer/src/main/java/com/edusoho/liveplayer/Utils.java
@@ -0,0 +1,26 @@
+package com.edusoho.liveplayer;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by suju on 16/10/12.
+ */
+public class Utils {
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    /**
+     * Is the live streaming still available
+     * @return is the live streaming is available
+     */
+    public static boolean isLiveStreamingAvailable() {
+        // Todo: Please ask your app server, is the live streaming still available
+        return true;
+    }
+}
