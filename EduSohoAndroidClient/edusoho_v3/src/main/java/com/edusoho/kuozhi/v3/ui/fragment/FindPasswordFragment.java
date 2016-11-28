1009331cba540565e264ab6551cc810796dbcd17
Huang Boyi <huangboyi@howzhi.com>
2016-11-28 15:42:29 +0800

    add reset password by phone

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/FindPasswordFragment.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/FindPasswordFragment.java
@@ -3,10 +3,14 @@ package com.edusoho.kuozhi.v3.ui.fragment;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.edusoho.kuozhi.R;
 import com.edusoho.kuozhi.v3.ui.ForgetPasswordActivity;
 import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
+import com.edusoho.kuozhi.v3.util.ToastUtil;
+import com.edusoho.kuozhi.v3.util.Validator;
 
 /**
  * Created by JesseHuang on 2016/11/25.
@@ -15,6 +19,7 @@ import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
 public class FindPasswordFragment extends BaseFragment {
 
     private Button btnNext;
+    private EditText etPhoneOrMail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,7 @@ public class FindPasswordFragment extends BaseFragment {
     protected void initView(View view) {
         super.initView(view);
         btnNext = (Button) view.findViewById(R.id.btn_next);
+        etPhoneOrMail = (EditText) view.findViewById(R.id.et_phone_or_mail);
         btnNext.setOnClickListener(getNextClickListener());
     }
 
@@ -33,9 +39,19 @@ public class FindPasswordFragment extends BaseFragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if ("".equals(etPhoneOrMail.getText().toString().trim())) {
+                    ToastUtil.getInstance(mContext).makeText(getString(R.string.find_password_text_not_null), Toast.LENGTH_LONG).show();
+                    return;
+                }
                 if (getActivity() != null && getActivity() instanceof ForgetPasswordActivity) {
                     ForgetPasswordActivity forgetPasswordActivity = (ForgetPasswordActivity) getActivity();
-                    forgetPasswordActivity.switchFragment("FindPasswordByPhoneFragment");
+                    if (Validator.isMail(etPhoneOrMail.getText().toString().trim())) {
+                        forgetPasswordActivity.switchFragment("FindPasswordByMailFragment");
+                    } else if (Validator.isPhone(etPhoneOrMail.getText().toString().trim())) {
+                        forgetPasswordActivity.switchFragment("FindPasswordByPhoneFragment");
+                    } else {
+                        ToastUtil.getInstance(mContext).makeText(getString(R.string.phone_or_mail_format_error), Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         };
