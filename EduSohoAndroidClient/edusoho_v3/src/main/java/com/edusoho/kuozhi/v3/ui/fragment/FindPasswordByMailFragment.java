868b5b851b28713c3ac8fad9d543a83dfae6c29b
Huang Boyi <huangboyi@howzhi.com>
2016-12-05 10:44:44 +0800

    add find password via email

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/FindPasswordByMailFragment.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/FindPasswordByMailFragment.java
@@ -1,6 +1,10 @@
 package com.edusoho.kuozhi.v3.ui.fragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.InputType;
 import android.view.View;
@@ -12,6 +16,9 @@ import android.widget.ImageView;
 
 import com.edusoho.kuozhi.R;
 import com.edusoho.kuozhi.v3.listener.NormalCallback;
+import com.edusoho.kuozhi.v3.listener.PluginRunCallback;
+import com.edusoho.kuozhi.v3.ui.ForgetPasswordActivity;
+import com.edusoho.kuozhi.v3.ui.LoginActivity;
 import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
 import com.edusoho.kuozhi.v3.util.InputUtils;
 
@@ -25,11 +32,18 @@ public class FindPasswordByMailFragment extends BaseFragment {
     private EditText etResetPassword;
     private ImageView ivErase;
     private CheckBox cbShowOrHidePassword;
+    private String mEmail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContainerView(R.layout.fragment_find_password_by_mail);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
     }
 
     @Override
@@ -56,11 +70,28 @@ public class FindPasswordByMailFragment extends BaseFragment {
         });
     }
 
+    private void initData() {
+        if (getArguments() != null && getArguments().getString(ForgetPasswordActivity.RESET_INFO) != null) {
+            mEmail = getArguments().getString(ForgetPasswordActivity.RESET_INFO);
+        }
+    }
+
     private View.OnClickListener getSubmitClickListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                new AlertDialog.Builder(getActivity()).setMessage("请前往该邮箱验证信息，验证成功即可登录").
+                        setPositiveButton("去登录", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                app.mEngine.runNormalPlugin("LoginActivity", mContext, new PluginRunCallback() {
+                                    @Override
+                                    public void setIntentDate(Intent startIntent) {
+                                        startIntent.putExtra(LoginActivity.FIND_PASSWORD_ACCOUNT, mEmail);
+                                    }
+                                }, Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            }
+                        }).show();
             }
         };
     }
