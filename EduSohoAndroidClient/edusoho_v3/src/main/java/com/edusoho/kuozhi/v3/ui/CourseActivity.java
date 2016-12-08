f213fab87a08b9b0b885ed75b02f6580a29a1a21
zhangruoyi <zhangruoyi@howzhi.com>
2016-12-08 11:51:34 +0800

    创建分支

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/CourseActivity.java
@@ -0,0 +1,23 @@
+package com.edusoho.kuozhi.v3.ui;
+
+import android.os.Bundle;
+
+import com.edusoho.kuozhi.v3.ui.base.BaseNoTitleActivity;
+
+/**
+ * Created by Zhang on 2016/12/8.
+ */
+
+public class CourseActivity extends BaseNoTitleActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+    }
+}
