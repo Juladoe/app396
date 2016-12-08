0f850a20444c0cf94ebea4d995d8480601b12c39
zhangruoyi <zhangruoyi@howzhi.com>
2016-12-08 17:49:54 +0800

    简介

---
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/CourseDetailFragment.java
@@ -0,0 +1,13 @@
+package com.edusoho.kuozhi.v3.ui.fragment;
+
+import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
+
+/**
+ * Created by Zhang on 2016/12/8.
+ */
+
+public class CourseDetailFragment extends BaseFragment {
+
+
+
+}
