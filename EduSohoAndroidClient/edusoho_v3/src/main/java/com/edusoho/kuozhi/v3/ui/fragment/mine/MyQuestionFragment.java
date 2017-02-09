fcc02c87ce9979304af0390b8d18b0b37d24bc80
Huang Boyi <huangboyi@howzhi.com>
2017-02-09 23:54:18 +0800

    add switch type in question fragment

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/mine/MyQuestionFragment.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/ui/fragment/mine/MyQuestionFragment.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 import com.edusoho.kuozhi.R;
 import com.edusoho.kuozhi.v3.EdusohoApp;
-import com.edusoho.kuozhi.v3.adapter.MyAskAdapter;
 import com.edusoho.kuozhi.v3.adapter.MyAskQuestionAdapter;
 import com.edusoho.kuozhi.v3.listener.NormalCallback;
 import com.edusoho.kuozhi.v3.model.bal.thread.MyThreadEntity;
@@ -16,11 +15,11 @@ import com.edusoho.kuozhi.v3.model.provider.MyThreadProvider;
 import com.edusoho.kuozhi.v3.model.sys.RequestUrl;
 import com.edusoho.kuozhi.v3.ui.base.BaseFragment;
 import com.edusoho.kuozhi.v3.util.Const;
+import com.edusoho.kuozhi.v3.view.EduSohoNewIconView;
 
 import org.sufficientlysecure.htmltextview.HtmlTextView;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by JesseHuang on 2017/2/8.
@@ -28,11 +27,18 @@ import java.util.List;
 
 public class MyQuestionFragment extends BaseFragment {
 
-    private RecyclerView rvFavorite;
+    private static int ASK = 1;
+    private static int ANSWER = 2;
+
+    private RecyclerView rvContent;
     private View viewEmpty;
     private View rlayoutFilterType;
     private View llayoutFilterQuestionTypeList;
     private View viewCoverScreen;
+    private TextView tvFilterName;
+    private EduSohoNewIconView esivFilterArrow;
+    private TextView tvAsk;
+    private TextView tvAnswer;
 
     private MyThreadProvider mMyThreadProvider;
 
@@ -53,29 +59,49 @@ public class MyQuestionFragment extends BaseFragment {
         viewEmpty = view.findViewById(R.id.view_empty);
         viewEmpty.setVisibility(View.GONE);
 
-        rvFavorite = (RecyclerView) view.findViewById(R.id.rv_content);
-        rvFavorite.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rvContent = (RecyclerView) view.findViewById(R.id.rv_content);
+        rvContent.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         rlayoutFilterType = view.findViewById(R.id.rlayout_filter_type);
         rlayoutFilterType.setVisibility(View.VISIBLE);
-        rlayoutFilterType.setOnClickListener(getFilterTypeClickListener());
+        rlayoutFilterType.setOnClickListener(getShowTypeLayoutClickListener());
 
         llayoutFilterQuestionTypeList = view.findViewById(R.id.llayout_filter_question_type_list);
         llayoutFilterQuestionTypeList.bringToFront();
 
         viewCoverScreen = view.findViewById(R.id.view_cover_screen);
         viewCoverScreen.setOnClickListener(getCoverScreenClickListener());
+
+        tvFilterName = (TextView) view.findViewById(R.id.tv_filter_name);
+
+        esivFilterArrow = (EduSohoNewIconView) view.findViewById(R.id.tv_filter_arrow);
+        tvAsk = (TextView) view.findViewById(R.id.tv_question_post);
+        tvAnswer = (TextView) view.findViewById(R.id.tv_question_answer);
+        tvAsk.setOnClickListener(getClickTypeClickListener());
+        tvAnswer.setOnClickListener(getClickTypeClickListener());
     }
 
     private void initData() {
         mMyThreadProvider = new MyThreadProvider(mContext);
-        loadAskQuestionData();
+        switchFilterType(ASK);
     }
 
-    private void loadAskQuestionData() {
+    private void loadAskedQuestionData() {
         RequestUrl requestUrl = EdusohoApp.app.bindNewUrl(Const.MY_CREATED_THREADS + "?start=0&limit=10000", true);
         final MyAskQuestionAdapter askQuestionAdapter = new MyAskQuestionAdapter(mContext);
-        rvFavorite.setAdapter(askQuestionAdapter);
+        rvContent.setAdapter(askQuestionAdapter);
+        mMyThreadProvider.getMyCreatedThread(requestUrl).success(new NormalCallback<MyThreadEntity[]>() {
+            @Override
+            public void success(MyThreadEntity[] entities) {
+                askQuestionAdapter.addDatas(Arrays.asList(entities));
+            }
+        });
+    }
+
+    private void loadAnsweredQuestionData() {
+        RequestUrl requestUrl = EdusohoApp.app.bindNewUrl(Const.MY_POSTED_THREADS + "?start=0&limit=10000", true);
+        final MyAskQuestionAdapter askQuestionAdapter = new MyAskQuestionAdapter(mContext);
+        rvContent.setAdapter(askQuestionAdapter);
         mMyThreadProvider.getMyCreatedThread(requestUrl).success(new NormalCallback<MyThreadEntity[]>() {
             @Override
             public void success(MyThreadEntity[] entities) {
@@ -87,21 +113,51 @@ public class MyQuestionFragment extends BaseFragment {
     private void setNoCourseDataVisible(boolean visible) {
         if (visible) {
             viewEmpty.setVisibility(View.VISIBLE);
-            rvFavorite.setVisibility(View.GONE);
+            rvContent.setVisibility(View.GONE);
         } else {
             viewEmpty.setVisibility(View.GONE);
-            rvFavorite.setVisibility(View.VISIBLE);
+            rvContent.setVisibility(View.VISIBLE);
         }
     }
 
-    private View.OnClickListener getFilterTypeClickListener() {
+    private void switchFilterType(int type) {
+        if (type == ASK) {
+            tvFilterName.setText(getString(R.string.question_post));
+            loadAskedQuestionData();
+            tvAsk.setTextColor(getResources().getColor(R.color.primary_color));
+            tvAnswer.setTextColor(getResources().getColor(R.color.primary_font_color));
+        } else if (type == ANSWER) {
+            tvFilterName.setText(getString(R.string.question_answer));
+            loadAnsweredQuestionData();
+            tvAsk.setTextColor(getResources().getColor(R.color.primary_font_color));
+            tvAnswer.setTextColor(getResources().getColor(R.color.primary_color));
+        }
+    }
+
+    private View.OnClickListener getClickTypeClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.tv_question_post) {
+                    switchFilterType(ASK);
+                } else if (v.getId() == R.id.tv_question_answer) {
+                    switchFilterType(ANSWER);
+                }
+                llayoutFilterQuestionTypeList.setVisibility(View.GONE);
+            }
+        };
+    }
+
+    private View.OnClickListener getShowTypeLayoutClickListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (llayoutFilterQuestionTypeList.getVisibility() == View.VISIBLE) {
                     llayoutFilterQuestionTypeList.setVisibility(View.GONE);
+                    esivFilterArrow.setText(getString(R.string.new_font_unfold));
                 } else {
                     llayoutFilterQuestionTypeList.setVisibility(View.VISIBLE);
+                    esivFilterArrow.setText(getString(R.string.new_font_fold));
                 }
             }
         };
@@ -138,11 +194,12 @@ public class MyQuestionFragment extends BaseFragment {
     }
 
     public static class ViewHolderAnswer extends RecyclerView.ViewHolder {
-        TextView tvTime;
-        HtmlTextView tvContentAnswer;
-        TextView tvContentAsk;
-        TextView tvOrder;
-        View vLine;
+        public TextView tvTime;
+        public HtmlTextView tvContentAnswer;
+        public TextView tvContentAsk;
+        public TextView tvOrder;
+        public View vLine;
+        public View layout;
 
         public ViewHolderAnswer(View view) {
             super(view);
@@ -150,6 +207,7 @@ public class MyQuestionFragment extends BaseFragment {
             tvContentAnswer = (HtmlTextView) view.findViewById(R.id.tv_content_answer);
             tvContentAsk = (TextView) view.findViewById(R.id.tv_content_ask);
             tvOrder = (TextView) view.findViewById(R.id.tv_order);
+            layout = view.findViewById(R.id.rlayout_answer_question_item_layout);
             vLine = view.findViewById(R.id.v_line);
         }
     }
