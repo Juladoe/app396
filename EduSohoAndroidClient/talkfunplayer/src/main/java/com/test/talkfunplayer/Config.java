f3a6fb57aa329ff18003e275bb976c7bb2465921
Huang Boyi <huangboyi@howzhi.com>
2017-02-22 14:12:17 +0800

    fix

--- EduSohoAndroidClient/talkfunplayer/src/main/java/com/test/talkfunplayer/Config.java
+++
@@ -1,9 +0,0 @@
-package com.test.talkfunplayer;
-
-/**
- * Created by asus on 2016/7/5.
- */
-public class Config {
-    public static String appid = "10000";
-    public static String accessKey = "8f584a00fd532dd436a7e2e0b8e21e86";
-}
