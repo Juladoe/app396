d78bea56679c1881d84469d241d052c40283cca9
dufan <df@163.com>
2017-03-18 16:32:34 +0800

    添加虚拟币支付dialog

--- EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/view/dialog/CustomDialog.java
+++ EduSohoAndroidClient/edusoho_v3/src/main/java/com/edusoho/kuozhi/v3/view/dialog/CustomDialog.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.edusoho.kuozhi.R;
@@ -14,21 +16,20 @@ import com.edusoho.kuozhi.R;
  * Created by DF on 2017/3/17.
  */
 
-public class StudyProgressDialog extends Dialog {
+public class CustomDialog extends Dialog {
 
     private String mAlreadyPlan;
     private String mPlanComplete;
     private String mCourseDate;
-    private TextView mTvAlreadyPlan;
-    private TextView mTvPlanCom;
-    private TextView mTvCourseDate;
     private int mType;
+    private String mAccountBalance;
+    private String mOrderAmount;
 
-    public StudyProgressDialog(@NonNull Context context) {
+    public CustomDialog(@NonNull Context context) {
         super(context, R.style.dialog_custom);
     }
 
-    public StudyProgressDialog(@NonNull Context context, @StyleRes int themeResId) {
+    public CustomDialog(@NonNull Context context, @StyleRes int themeResId) {
         super(context, themeResId);
     }
 
@@ -40,7 +41,12 @@ public class StudyProgressDialog extends Dialog {
         initEvent();
     }
 
-    public StudyProgressDialog initType(int type) {
+    /**
+     * 初始化dialog的类型1:学习进度 2:支付方式 3:优惠券 4:虚拟币 5:输入支付密码
+     * @param type
+     * @return
+     */
+    public CustomDialog initType(int type) {
         mType = type;
         return this;
     }
@@ -64,20 +70,26 @@ public class StudyProgressDialog extends Dialog {
     private void initView() {
         switch (mType) {
             case 1:
-                if (mTvAlreadyPlan == null) {
-                    mTvAlreadyPlan = (TextView) findViewById(R.id.already_plan);
-                    mTvPlanCom = (TextView) findViewById(R.id.plan_complete);
-                    mTvCourseDate = (TextView) findViewById(R.id.course_date);
-                }
-                mTvAlreadyPlan.setText(String.format("%s%s","已经完成计划: ", mAlreadyPlan));
-                mTvPlanCom.setText(String.format("%s%s", "计划完成任务: ", mPlanComplete));
-                mTvCourseDate.setText(String.format("%s%s", "课程有效期至: ", mCourseDate));
+                TextView tvAlreadyPlan = (TextView) findViewById(R.id.already_plan);
+                TextView tvPlanCom = (TextView) findViewById(R.id.plan_complete);
+                TextView tvCourseDate = (TextView) findViewById(R.id.course_date);
+                tvAlreadyPlan.setText(String.format("%s%s","已经完成计划: ", mAlreadyPlan));
+                tvPlanCom.setText(String.format("%s%s", "计划完成任务: ", mPlanComplete));
+                tvCourseDate.setText(String.format("%s%s", "课程有效期至: ", mCourseDate));
                 break;
             case 2:
                 break;
             case 3:
                 break;
             case 4:
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                lp.width = getContext().getResources().getDisplayMetrics().widthPixels;
+                getWindow().setGravity(Gravity.BOTTOM);
+                getWindow().setAttributes(lp);
+                TextView tvAccountBalance = (TextView) findViewById(R.id.tv_account_balance);
+                TextView tvOrderAmount = (TextView) findViewById(R.id.tv_order_amount);
+                tvAccountBalance.setText(mAccountBalance);
+                tvOrderAmount.setText(mOrderAmount);
                 break;
             case 5:
                 break;
@@ -108,6 +120,15 @@ public class StudyProgressDialog extends Dialog {
             case 3:
                 break;
             case 4:
+                findViewById(R.id.tv_confirm).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dismiss();
+                        // TODO: 2017/3/18
+                    }
+                });
+                break;
+            case 5:
                 break;
         }
 
@@ -130,15 +151,22 @@ public class StudyProgressDialog extends Dialog {
 
     /**
      * 初始化学习进度dialog中的信息
-     * @param alreadyPlan  已经完成计划
-     * @param planComplete   计划完成任务
-     * @param courseDate   课程有效期至
      * @return
      */
-    public Dialog initText(String alreadyPlan, String planComplete, String courseDate) {
-        this.mAlreadyPlan = alreadyPlan;
-        this.mPlanComplete = planComplete;
-        this.mCourseDate = courseDate;
+    public Dialog initText(String... text) {
+        switch (mType) {
+            case 1:
+                //已完成计划，计划完成任务，课程有效期至
+                this.mAlreadyPlan = text[0];
+                this.mPlanComplete = text[1];
+                this.mCourseDate = text[2];
+                break;
+            case 4:
+                //账户余额和订单金额
+                mAccountBalance = text[0];
+                mOrderAmount = text[1];
+                break;
+        }
         return this;
     }
 
